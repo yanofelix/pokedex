645737d3bcda0957697a91d9abc8a80dd197b311
Yan Oliveira <yoliveira@indracompany.com>
2022-02-24 23:15:56 -0300

    listando pokemons

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent implements OnInit {
+  pokemons: any[] = [];
+
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  ngOnInit(): void {
+    this.dataService.getPokemons()
+    .subscribe((response: any) => {
+      response.results.forEach((result: { name: string; }) => {
+        this.dataService.getMoreData(result.name)
+        .subscribe((uniqResponse: any) => {
+          this.pokemons.push(uniqResponse);
+          console.log(this.pokemons);
+        });
+      });
+    });
+  }
+
+}
